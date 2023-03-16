5f56e2928d7a23cffc4af288526f898d840a503c
Noi-Git <noi.patsin@gmail.com>
2023-03-16 18:49:09 -0400

    createTodo() to add input data to the array

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import { useState } from 'react'
+import uuid from 'react-uuid'
 import './App.css'
-import TodoList from './components/TodoList'
+import TodoCreate from './components/TodoCreate'
 
 function App() {
   const [todos, setTodos] = useState([])
 
-  const handleTodoChange = (event) => {
-    setTodos(event.target.value)
-    console.log(event.target.value)
+  const createTodo = (todoTitle) => {
+    const updatedTodo = [...todos, { id: uuid(), todoTitle }]
+    setTodos(updatedTodo)
   }
 
   return (
     <div className='App'>
-      <div className='app-main'>
-        <form className='app-todo'>
-          <div>
-            <h1>Add your todo</h1>
-            <div className='input-field'>
-              <input
-                className='todo-input'
-                type='text'
-                value={todos}
-                onChange={handleTodoChange}
-              />
-              <button className='todo-button'>Add</button>
-            </div>
-          </div>
-        </form>
-        <TodoList todos={todos} />
-      </div>
+      <TodoCreate onCreate={createTodo} />
     </div>
   )
 }
