e9bab1cc71e52edd5277d15774b3ed84e54244b3
Noi-Git <noi.patsin@gmail.com>
2023-03-16 15:57:29 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import TodoList from './components/TodoList'
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const handleTodoChange = (event) => {
+    setTodos(event.target.value)
+    console.log(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='app-main'>
+        <form className='app-todo'>
+          <div>
+            <h1>Add your todo</h1>
+            <div className='input-field'>
+              <input
+                className='todo-input'
+                type='text'
+                value={todos}
+                onChange={handleTodoChange}
+              />
+              <button className='todo-button'>Add</button>
+            </div>
+          </div>
+        </form>
+        <TodoList todos={todos} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TodoList = ({ todos }) => {
+  return <div className='todo-list'>{todos}</div>
+}
+
+export default TodoList
